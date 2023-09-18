2abb1e87a5901ab54838674b7c0dd826aad3b180
Mohammed Sharique <msharique@microsoft.com>
2023-09-18 18:58:05 +0530

    post call setup

--- src/Components/UrlForm/index.js
+++ src/Components/UrlForm/index.js
@@ -1,25 +1,35 @@
+import * as React from 'react';
 import "./style.scss";
 import BackgroundImg from "../../assets/img/svgexport-1.svg";
 import { MonitorHeart, YouTube } from "@mui/icons-material";
-import { Button, TextField } from "@mui/material";
+import { Button, Snackbar, TextField } from "@mui/material";
 
-const UrlForm = () => {
+const UrlForm = ({ showtoast, handleSubmit }) => {
+  const [url, setUrl] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  
   return (
     <div className="url-form">
       <img src={BackgroundImg} alt="podcast" />
       <div className="form">
         <h1>Summarize, Simplify, Streamline Your YouTube Experience with Podcast Pulse 📺🔊</h1>
         <YouTube color="error" fontSize="large" />
-        <form action="#">
-          <TextField fullWidth label="" id="fullWidth" placeholder="https:// ..." />
-          <TextField fullWidth label="" id="fullWidthEmail" placeholder="Email ...@..." />
+        <div className="div-form">
+          <TextField fullWidth value={url} label="https://" id="fullWidth" onChange={evt => setUrl(evt.target.value)} placeholder="https:// ..." />
+          <TextField fullWidth value={email} label="Email" id="fullWidthEmail" onChange={evt => setEmail(evt.target.value)} placeholder="Email ...@..." />
           <Button
             variant="contained"
             type="submit"
+            onClick={() => handleSubmit(url, email)}
             startIcon={<MonitorHeart />}>
             Get Pulse
           </Button>
-        </form>
+        </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          open={showtoast}
+          message="Your summary request is in! 🚀 We're working on it and will send you the summary via email soon."
+        />
       </div>
     </div>
   )
--- src/Container/Home.js
+++ src/Container/Home.js
@@ -1,14 +1,35 @@
+import * as React from 'react';
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
-// import InProgress from "../Components/Inprogress";
+import InProgress from "../Components/Inprogress";
 import UrlForm from "../Components/UrlForm";
+import axios from 'axios';
 
 const Home = () => {
+  const [inProgress, setInProgress] = React.useState(false);
+  const [showToast, setShowToast] = React.useState(false);
+  
+  const handleSubmit = (url, email) => {
+    setInProgress(true);
+    axios.post("URL", { url, email})
+      .then(_ => {
+        setShowToast(true);
+      })
+      .catch(err => {
+        console.log("err:: ", err);
+      })
+      .finally(() => {
+        setInProgress(false);
+        setTimeout(() => {
+          setShowToast(false);
+        }, 5000)
+      })
+  }
+
   return (
     <>
       <Header />
-      <UrlForm />
-      {/* <InProgress /> */}
+      {inProgress ? <InProgress /> : <UrlForm showtoast={showToast} handleSubmit={handleSubmit} />}
       <Footer />
     </>
   )
